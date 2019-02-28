01b637f454dd6db4cabc751a007a0f00e9af0b82
Arif Hidayat <arif.h@mail.ugm.ac.id>
2019-02-28 19:44:59 +0700

    second

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -226,7 +226,7 @@
     {{-- <script src="{{ asset('/cooladmin/vendor/bootstrap-4.1/popper.min.js') }}"></script> --}}
     <script src="{{ asset('/cooladmin/vendor/bootstrap-4.1/bootstrap.min.js') }}"></script>
     <!-- Vendor JS       -->
-    {{-- {{-- <script src="{{ asset('/cooladmin/vendor/slick/slick.min.js') }}"> --}}
+    {{-- {{-- <script src="{{ asset('/cooladmin/vendor/slick/slick.min.js') }}">
     </script> --}}
     {{-- <script src="{{ asset('/cooladmin/vendor/wow/wow.min.js') }}"></script> --}}
     <script src="{{ asset('/cooladmin/vendor/animsition/animsition.min.js') }}"></script>
@@ -238,8 +238,8 @@
     {{-- <script src="{{ asset('/cooladmin/vendor/circle-progress/circle-progress.min.js') }}"></script> --}}
     <script src="{{ asset('/cooladmin/css/font-face.css') }}"></script>
     {{-- <script src="{{ asset('/cooladmin/vendor/chartjs/Chart.bundle.min.js') }}"></script>
-    <script src="{{ asset('/cooladmin/vendor/select2/select2.min.js') }}"> --}}
-    </script>
+    <script src="{{ asset('/cooladmin/vendor/select2/select2.min.js') }}">
+    </script>--}}
 
     <!-- Main JS-->
     <script src="{{ asset('/cooladmin/js/main.js')}}"></script>
